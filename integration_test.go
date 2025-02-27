ec3dabacbc98e5a7a3fac768178b1ead8204f24c
William Lim <limwilliam23@gmail.com>
2025-02-26 21:01:26 -0500

    feat: user management

--- integration_test.go
+++ integration_test.go
@@ -1,6 +1,3 @@
-//go:build integration
-// +build integration
-
 package main
 
 import (
@@ -17,13 +14,13 @@ import (
 
 func setupTestApi(t humatest.TB) humatest.TestAPI {
 	app := App{
-		db: setupDB(os.Getenv("DB_URL")),
+		st: setupDB(os.Getenv("DB_URL")),
 	}
-	s, _ := sqids.New(sqids.Options{
-		MinLength: 9,
-	})
-	app.s = s
+	app.parser = NewParser()
 
+	app.st.CreateTestUser("testid", "admin", "admin@admin.admin")
+	app.st.CreateTestUser("testid2", "player2", "a@admin.admin")
+	app.st.CreateTestUser("testid3", "player3", "s@admin.admin")
 	_, api := humatest.New(t)
 	app.addRoutes(api)
 	return api
@@ -34,16 +31,24 @@ func getLeaderboard(api humatest.TestAPI, leaderboard_id string) (LeaderboardRes
 	var lResp LeaderboardResponseBody
 	json.Unmarshal(getResp.Body.Bytes(), &lResp)
 	return lResp, getResp
+}
 
+func getAccountLeaderboards(api humatest.TestAPI, user_id string) (AccountLeaderboardsResponseBody, *httptest.ResponseRecorder) {
+	getResp := api.Get(fmt.Sprintf("/account/%s/leaderboards", user_id))
+	var lResp AccountLeaderboardsResponseBody
+	json.Unmarshal(getResp.Body.Bytes(), &lResp)
+	return lResp, getResp
 }
 
 func TestDisplayName(t *testing.T) {
 	api := setupTestApi(t)
 	display_name := "Test Leaderboard Name"
 
-	resp := api.Post("/leaderboard", map[string]any{
-		"name": display_name,
-	})
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": display_name,
+		})
 
 	var newResp NewLeaderboardResponseBody
 	json.Unmarshal(resp.Body.Bytes(), &newResp)
@@ -58,7 +63,6 @@ func TestDisplayName(t *testing.T) {
 	json.Unmarshal(getNameResp.Body.Bytes(), &lResp)
 
 	assert.Equal(t, display_name, lResp.Name)
-
 }
 
 func TestGetBadIDEmpty(t *testing.T) {
@@ -82,12 +86,243 @@ func TestGetBadIDTooLong(t *testing.T) {
 	assert.Equal(t, 422, getResp.Code)
 }
 
+func TestUpdateSubmissionBecomesUnverified(t *testing.T) {
+	api := setupTestApi(t)
+
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
+	assert.Equal(t, 200, resp.Code)
+	var newResp NewLeaderboardResponseBody
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
+	assert.Equal(t, "test name", newResp.Name)
+
+	postResp2 := api.Post(
+		fmt.Sprintf("/leaderboard/%s/submission", newResp.Id),
+		"UserID: testid2",
+		map[string]any{
+			"score": 10,
+		})
+
+	assert.Equal(t, 200, postResp2.Code)
+
+	if lResp, getResp := getLeaderboard(api, newResp.Id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 10, lResp.Scores[0].Score)
+		assert.False(t, lResp.Scores[0].Verified)
+	}
+
+	var submissionBody SubmissionResponseBody
+	json.Unmarshal(postResp2.Body.Bytes(), &submissionBody)
+	updateResp := api.Patch(
+		fmt.Sprintf("/leaderboard/%s/submission/%s/verify", newResp.Id, submissionBody.ID),
+		"UserID: testid",
+		map[string]any{
+			"is_valid": true,
+		})
+	assert.Equal(t, 200, updateResp.Code)
+
+	if lResp, getResp := getLeaderboard(api, newResp.Id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 10, lResp.Scores[0].Score)
+		assert.True(t, lResp.Scores[0].Verified)
+	}
+
+	var newScoreBody SubmissionResponseBody
+	json.Unmarshal(postResp2.Body.Bytes(), &newScoreBody)
+	updateRespScore := api.Patch(
+		fmt.Sprintf("/leaderboard/%s/submission/%s/score", newResp.Id, newScoreBody.ID),
+		"UserID: testid",
+		map[string]any{
+			"score": 100,
+		})
+
+	assert.Equal(t, 200, updateRespScore.Code)
+
+	if lResp, getResp := getLeaderboard(api, newResp.Id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 100, lResp.Scores[0].Score)
+		assert.False(t, lResp.Scores[0].Verified)
+	}
+}
+
+func TestVerifyScoreNotOwner(t *testing.T) {
+	api := setupTestApi(t)
+
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
+	assert.Equal(t, 200, resp.Code)
+	var newResp NewLeaderboardResponseBody
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
+	assert.Equal(t, "test name", newResp.Name)
+
+	postResp2 := api.Post(
+		fmt.Sprintf("/leaderboard/%s/submission", newResp.Id),
+		"UserID: testid2",
+		map[string]any{
+			"score": 10,
+		})
+
+	assert.Equal(t, 200, postResp2.Code)
+
+	if lResp, getResp := getLeaderboard(api, newResp.Id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 10, lResp.Scores[0].Score)
+		assert.False(t, lResp.Scores[0].Verified)
+	}
+
+	var newScoreBody SubmissionResponseBody
+	json.Unmarshal(postResp2.Body.Bytes(), &newScoreBody)
+	updateResp := api.Patch(
+		fmt.Sprintf("/leaderboard/%s/submission/%s/verify", newResp.Id, newScoreBody.ID),
+		"UserID: testid2",
+		map[string]any{
+			"is_valid": true,
+		})
+	assert.Equal(t, 401, updateResp.Code)
+
+}
+
+func TestVerifyScore(t *testing.T) {
+	api := setupTestApi(t)
+
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
+	assert.Equal(t, 200, resp.Code)
+	var newResp NewLeaderboardResponseBody
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
+	assert.Equal(t, "test name", newResp.Name)
+
+	postResp2 := api.Post(
+		fmt.Sprintf("/leaderboard/%s/submission", newResp.Id),
+		"UserID: testid2",
+		map[string]any{
+			"score": 10,
+		})
+
+	assert.Equal(t, 200, postResp2.Code)
+
+	if lResp, getResp := getLeaderboard(api, newResp.Id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 10, lResp.Scores[0].Score)
+		assert.False(t, lResp.Scores[0].Verified)
+	}
+
+	var newScoreBody SubmissionResponseBody
+	json.Unmarshal(postResp2.Body.Bytes(), &newScoreBody)
+	updateResp := api.Patch(
+		fmt.Sprintf("/leaderboard/%s/submission/%s/verify", newResp.Id, newScoreBody.ID),
+		"UserID: testid",
+		map[string]any{
+			"is_valid": true,
+		})
+	assert.Equal(t, 200, updateResp.Code)
+
+	if lResp, getResp := getLeaderboard(api, newResp.Id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 10, lResp.Scores[0].Score)
+		assert.True(t, lResp.Scores[0].Verified)
+	}
+
+}
+
+func TestGetUserLeaderboards(t *testing.T) {
+	api := setupTestApi(t)
+
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
+	assert.Equal(t, 200, resp.Code)
+	var newResp NewLeaderboardResponseBody
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
+	assert.Equal(t, "test name", newResp.Name)
+
+	if lResp, getResp := getAccountLeaderboards(api, "testid"); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Leaderboards))
+	}
+
+	resp2 := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name 2",
+		})
+	assert.Equal(t, 200, resp2.Code)
+	var newResp2 NewLeaderboardResponseBody
+	json.Unmarshal(resp2.Body.Bytes(), &newResp2)
+	assert.Equal(t, "test name 2", newResp2.Name)
+
+	if lResp, getResp := getAccountLeaderboards(api, "testid"); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 2, len(lResp.Leaderboards))
+	}
+}
+
+func TestUpdateScore(t *testing.T) {
+	api := setupTestApi(t)
+
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
+	assert.Equal(t, 200, resp.Code)
+	var newResp NewLeaderboardResponseBody
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
+	assert.Equal(t, "test name", newResp.Name)
+
+	id := newResp.Id
+
+	if lResp, getResp := getLeaderboard(api, id); assert.Equal(t, 200, getResp.Code) {
+		assert.Zero(t, len(lResp.Scores))
+	}
+
+	postResp2 := api.Post(
+		fmt.Sprintf("/leaderboard/%s/submission", id),
+		"UserID: testid2",
+		map[string]any{
+			"score": 10,
+		})
+
+	assert.Equal(t, 200, postResp2.Code)
+
+	if lResp, getResp := getLeaderboard(api, id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 10, lResp.Scores[0].Score)
+	}
+
+	var newScoreBody SubmissionResponseBody
+	json.Unmarshal(postResp2.Body.Bytes(), &newScoreBody)
+	postResp3 := api.Patch(
+		fmt.Sprintf("/leaderboard/%s/submission/%s/score", id, newScoreBody.ID),
+		"UserID: testid2",
+		map[string]any{
+			"score": 11,
+		})
+	assert.Equal(t, 200, postResp3.Code)
+
+	if lResp, getResp := getLeaderboard(api, id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(lResp.Scores))
+		assert.Equal(t, 11, lResp.Scores[0].Score)
+	}
+}
+
 func TestAddScores(t *testing.T) {
 	api := setupTestApi(t)
 
-	resp := api.Post("/leaderboard", map[string]any{
-		"name": "test name",
-	})
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
 	assert.Equal(t, 200, resp.Code)
 	var newResp NewLeaderboardResponseBody
 	json.Unmarshal(resp.Body.Bytes(), &newResp)
@@ -100,9 +335,9 @@ func TestAddScores(t *testing.T) {
 	}
 
 	postResp := api.Post(
-		fmt.Sprintf("/leaderboard/%s/score", id),
+		fmt.Sprintf("/leaderboard/%s/submission", id),
+		"UserID: testid",
 		map[string]any{
-			"user":  "test",
 			"score": 9,
 		})
 
@@ -114,23 +349,26 @@ func TestAddScores(t *testing.T) {
 	}
 
 	postResp2 := api.Post(
-		fmt.Sprintf("/leaderboard/%s/score", id),
+		fmt.Sprintf("/leaderboard/%s/submission", id),
+		"UserID: testid2",
 		map[string]any{
-			"user":  "test user 2",
 			"score": 10,
 		})
 
 	assert.Equal(t, 200, postResp2.Code)
 
 	if lResp, getResp := getLeaderboard(api, id); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 2, len(lResp.Scores))
 		assert.Equal(t, 10, lResp.Scores[0].Score)
 		assert.Equal(t, 9, lResp.Scores[1].Score)
 	}
 
-	postResp3 := api.Post(
-		fmt.Sprintf("/leaderboard/%s/score", id),
+	var newScoreBody SubmissionResponseBody
+	json.Unmarshal(postResp2.Body.Bytes(), &newScoreBody)
+	postResp3 := api.Patch(
+		fmt.Sprintf("/leaderboard/%s/submission/%s/score", id, newScoreBody.ID),
+		"UserID: testid2",
 		map[string]any{
-			"user":  "test user 2",
 			"score": 11,
 		})
 	assert.Equal(t, 200, postResp3.Code)
@@ -140,12 +378,14 @@ func TestAddScores(t *testing.T) {
 		assert.Equal(t, 11, lResp.Scores[0].Score)
 		assert.Equal(t, 9, lResp.Scores[1].Score)
 	}
+
 	postResp4 := api.Post(
-		fmt.Sprintf("/leaderboard/%s/score", id),
+		fmt.Sprintf("/leaderboard/%s/submission", id),
+		"UserID: testid3",
 		map[string]any{
-			"user":  "test user 3",
 			"score": 11,
 		})
+
 	assert.Equal(t, 200, postResp4.Code)
 
 	if lResp, getResp := getLeaderboard(api, id); assert.Equal(t, 200, getResp.Code) {
@@ -159,12 +399,14 @@ func TestAddScores(t *testing.T) {
 func BenchmarkGetLeaderboard(b *testing.B) {
 	api := setupTestApi(b)
 
-	resp := api.Post("/leaderboard", map[string]any{
-		"name": "test leaderboard",
-	})
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test leaderboard",
+		})
 
 	var newResp NewLeaderboardResponseBody
-	json.Unmarshal(resp.Body.Bytes(), newResp)
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
 	s, _ := sqids.New()
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
@@ -172,9 +414,9 @@ func BenchmarkGetLeaderboard(b *testing.B) {
 		id, _ := s.Encode([]uint64{uint64(n)})
 
 		api.Post(
-			fmt.Sprintf("/leaderboard/%s/score", id),
+			fmt.Sprintf("/leaderboard/%s/submission", id),
+			"UserID: testid3",
 			map[string]any{
-				"user":  id,
 				"score": n,
 			})
 
