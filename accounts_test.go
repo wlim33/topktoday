cc8da0c314770302303ba82c3de101173031cabe
William Lim <limwilliam23@gmail.com>
2025-03-02 16:26:42 -0500

    fix: db query for submissions

--- accounts_test.go
+++ accounts_test.go
@@ -20,6 +20,13 @@ func getAccountLeaderboards(api humatest.TestAPI, user_id string) (AccountLeader
 	return lResp, getResp
 }
 
+func getAccountSubmissions(api humatest.TestAPI, user_id string) (AccountSubmissionsResponseBody, *httptest.ResponseRecorder) {
+	getResp := api.Get(fmt.Sprintf("/account/%s/submissions", user_id))
+	var lResp AccountSubmissionsResponseBody
+	json.Unmarshal(getResp.Body.Bytes(), &lResp)
+	return lResp, getResp
+}
+
 func TestGetUserLeaderboards(t *testing.T) {
 	api := setupTestApi(t)
 
@@ -35,6 +42,7 @@ func TestGetUserLeaderboards(t *testing.T) {
 
 	if lResp, getResp := getAccountLeaderboards(api, "testid"); assert.Equal(t, 200, getResp.Code) {
 		assert.Equal(t, 1, len(lResp.Leaderboards))
+		assert.Equal(t, "test name", lResp.Leaderboards[0].Title)
 	}
 
 	resp2 := api.Post("/leaderboard",
@@ -50,6 +58,51 @@ func TestGetUserLeaderboards(t *testing.T) {
 	if lResp, getResp := getAccountLeaderboards(api, "testid"); assert.Equal(t, 200, getResp.Code) {
 		assert.Equal(t, 2, len(lResp.Leaderboards))
 	}
+}
+
+func TestGetUserSubmissions(t *testing.T) {
+	api := setupTestApi(t)
+
+	resp := api.Post("/leaderboard",
+		"UserID: testid",
+		map[string]any{
+			"name": "test name",
+		})
+	assert.Equal(t, 200, resp.Code)
+	var newResp NewLeaderboardResponseBody
+	json.Unmarshal(resp.Body.Bytes(), &newResp)
+	assert.Equal(t, "test name", newResp.Name)
+
+	subResp := api.Post(
+		fmt.Sprintf("/leaderboard/%s/submission", newResp.Id),
+		"UserID: testid",
+		map[string]any{
+			"score": 10,
+			"link":  "www.youtube.com",
+		})
+
+	assert.Equal(t, 200, subResp.Code)
+	if submissionResp, getResp := getAccountSubmissions(api, "testid"); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 1, len(submissionResp.Submissions))
+		assert.Equal(t, 10, submissionResp.Submissions[0].Score)
+	}
+
+	submissionResp2 := api.Post(
+		fmt.Sprintf("/leaderboard/%s/submission", newResp.Id),
+		"UserID: testid",
+		map[string]any{
+			"score": 11,
+			"link":  "www.youtube.com/1",
+		})
+
+	assert.Equal(t, 200, submissionResp2.Code)
+
+	if submissionResp, getResp := getAccountSubmissions(api, "testid"); assert.Equal(t, 200, getResp.Code) {
+		assert.Equal(t, 2, len(submissionResp.Submissions))
+		assert.Equal(t, 10, submissionResp.Submissions[1].Score)
+		assert.Equal(t, 11, submissionResp.Submissions[0].Score)
+	}
+
 }
 
 func TestLinkAnonymousAccount(t *testing.T) {
