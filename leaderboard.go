93f4fee670b18c97dba91864a9af84bb47a377cd
William Lim <limwilliam23@gmail.com>
2025-03-09 11:47:12 -0400

    fix: docs actions

--- leaderboard.go
+++ leaderboard.go
@@ -95,9 +95,6 @@ func OpenAPIGenConfig() huma.Config {
 	config.Servers = []*huma.Server{
 		{URL: url},
 	}
-	// if !(len(DOCS) > 0) && !(len(CLI) > 0) {
-	// 	config.DocsPath = ""
-	// }
 
 	return config
 }
@@ -160,6 +157,7 @@ type Options struct {
 func main() {
 	log.Printf("app version: %s", VERSION)
 	port, db_url, ls_secret, api_key := os.Getenv("PORT"), os.Getenv("DB_URL"), os.Getenv("LS_SECRET"), os.Getenv("PAYMENT_API_KEY")
+
 	app := App{
 		log:         &logging.Logger{},
 		parser:      NewParser(),
@@ -179,52 +177,46 @@ func main() {
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
+
 	api := humachi.New(r, OpenAPIGenConfig())
 	app.addRoutes(api)
 
-	if len(CLI) > 0 {
-		cli := humacli.New(func(hooks humacli.Hooks, opts *Options) {
-			fmt.Printf("I was run with debug:%v host:%v port%v\n",
-				opts.Debug, opts.Host, opts.Port)
-			server := http.Server{
-				Addr:    fmt.Sprintf(":%d", opts.Port),
-				Handler: r,
-			}
-
-			hooks.OnStart(func() {
-				// Start your server here
-				server.ListenAndServe()
-			})
-
-			hooks.OnStop(func() {
-				// Gracefully shutdown your server here
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
-				server.Shutdown(ctx)
-			})
-		})
-		cli.Root().AddCommand(&cobra.Command{
-			Use:   "openapi",
-			Short: "Print the OpenAPI spec",
-			Run: func(cmd *cobra.Command, args []string) {
-				b, err := app.api.OpenAPI().YAML()
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(b))
-			}})
-		cli.Run()
-		return
-	} else {
-		if port == "" {
-			port = "8080"
-			log.Printf("defaulting to port %s", port)
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	cli := humacli.New(func(hooks humacli.Hooks, opts *Options) {
+		server := http.Server{
+			Addr:    fmt.Sprintf(":%d", opts.Port),
+			Handler: r,
 		}
 
-		app.st = setupDB(context.Background(), db_url)
-		defer app.st.Close()
-	}
-	if err := http.ListenAndServe(":"+port, r); err != nil {
-		log.Fatal(err)
-	}
+		hooks.OnStart(func() {
+			// Start your server here
+			app.st = setupDB(context.Background(), db_url)
+
+			if err := http.ListenAndServe(":"+port, r); err != nil {
+				log.Fatal(err)
+			}
+		})
+
+		hooks.OnStop(func() {
+			// Gracefully shutdown your server here
+			app.st.Close()
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			server.Shutdown(ctx)
+		})
+	})
+	cli.Root().AddCommand(&cobra.Command{
+		Use:   "openapi",
+		Short: "Print the OpenAPI spec",
+		Run: func(cmd *cobra.Command, args []string) {
+			b, err := app.api.OpenAPI().YAML()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(b))
+		}})
+	cli.Run()
 }
